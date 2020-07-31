93165b8931b7181e1db3ffaf36e42023624b6e46
Frances Cheng <francesvcheng@gmail.com>
2020-07-30 17:28:39 -0700

    added placeholder about text

--- src/about.jsx
+++ src/about.jsx
@@ -4,7 +4,35 @@ import React, { Component } from "react";
 function About(props) {
   return (
     <div className="about-info">
-      <h3>Hi, I’m Frances Cheng</h3>
+      <h3 id="hello">Hi, I’m Frances Cheng</h3>
+
+      <div className="info-block">
+        <div className="info-title">
+          <h3>I'm a:</h3>
+        </div>
+        <div className="info-des">
+          <p>Developer, Designer, Illustrator, Photographer, and Learner</p>
+        </div>
+      </div>
+
+      <div className="info-block">
+        <div className="info-title">
+          <h3>I've worked at:</h3>
+        </div>
+        <div className="info-des">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+      </div>
+
+      <div className="info-block">
+        <div className="info-title">
+          <h3>In my spare time<br/>I like to:</h3>
+        </div>
+        <div className="info-des">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+      </div>
+
     </div>
   )
 }
