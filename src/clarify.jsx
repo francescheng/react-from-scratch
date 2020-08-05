e2304d3b4135b61b71658e0ed9570f67deff4c4a
Frances Cheng <francesvcheng@gmail.com>
2020-08-05 16:16:19 -0700

    added components for projects and hover for navlinks

--- src/clarify.jsx
+++ src/clarify.jsx
@@ -6,7 +6,7 @@ function Clarify(props) {
   const { projectId } = useParams();
   return (
     <div>
-      <h3>Hello World</h3>
+      <h3>CLARIFY</h3>
       <p>My projectId is {projectId}.</p>
     </div>
   );
