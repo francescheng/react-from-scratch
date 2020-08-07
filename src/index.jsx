1a3e185f49ca094d360f195c5072482528349872
Frances Cheng <francesvcheng@gmail.com>
2020-08-07 13:36:26 -0700

    force refresh when navigating

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import App from "./App.jsx";
 
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter forceRefresh={true} basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById("root"));
 
 console.log("This is your", process.env.PUBLIC_URL);
 
