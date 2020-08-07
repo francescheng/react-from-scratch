70061e82ddad064bc40e302680c5131ac8a6b5e7
Frances Cheng <francesvcheng@gmail.com>
2020-08-07 10:42:32 -0700

    added image to ppf services page unstyled

--- src/ppfservices.jsx
+++ src/ppfservices.jsx
@@ -1,13 +1,18 @@
 import { hot } from 'react-hot-loader/root';
 import React, { Component } from "react";
 import { useParams } from "react-router-dom";
+import ppfimg from "../assets/images/ppf-mockup.jpg"
 
 function Ppf(props) {
   const { projectId } = useParams();
   return (
     <div>
-      <h3>Hello World</h3>
-      <p>My projectId is {projectId}.</p>
+    <div className="hello">
+        <h3>PPF Services Website</h3>
+        <p><strong>Brief:</strong> A client needed a website designed and created for his landscaping and housekeeping company PPF Services</p>
+    </div>
+        <img src={ppfimg} alt="PPF Services"/>
+
     </div>
   );
 }
